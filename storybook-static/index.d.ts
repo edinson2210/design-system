55edc6ac2c64fef727fd5ac924388ac082df3720
Edinson López <Edinson.221093@gmail.com>
2025-08-10 21:10:29 -0500

    feat: update Script

--- storybook-static/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { Button } from './components/Button/Button';
-export type { ButtonProps } from './components/Button/Button';
-export { Input } from './components/Input/Input';
-export type { InputProps } from './components/Input/Input';
-export { Card } from './components/Card/Card';
-export type { CardProps } from './components/Card/Card';
-//# sourceMappingURL=index.d.ts.map
