0a309ac4c22a2f1f2496272a7d68b08632659d21
Edinson López <Edinson.221093@gmail.com>
2025-08-10 20:56:49 -0500

    feat: Deploy Storybook to GitHub Pages

---
+++ storybook-static/stories/Button.stories.d.ts
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Button } from '../components/Button/Button';
+declare const meta: Meta<typeof Button>;
+export default meta;
+type Story = StoryObj<typeof Button>;
+export declare const Playground: Story;
+export declare const VariantsGrid: Story;
+export declare const Sizes: Story;
+export declare const Radius: Story;
+export declare const AsLink: Story;
+export declare const Loading: Story;
+export declare const LoadingEnd: Story;
+export declare const IconOnly: Story;
+export declare const Disabled: Story;
+export declare const FullWidth: Story;
+//# sourceMappingURL=Button.stories.d.ts.map
